ad2722637625e243cf5ae88596e5e8abd057f02e
Jayaprakash Elavarasan <jayaprakash@solbaacken.com>
2020-06-19 20:03:35 +0530

    Updates

---
+++ precache-manifest.915807ff57007fe022bf5686b34701e0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1f3b7b191f2b8770cc3d187a4aa906",
+    "url": "/guidance-task/index.html"
+  },
+  {
+    "revision": "ba41fcace7cb0b2c4ea3",
+    "url": "/guidance-task/static/css/main.98f62665.chunk.css"
+  },
+  {
+    "revision": "6db31f869601d0bf5b37",
+    "url": "/guidance-task/static/js/2.7d0bb843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guidance-task/static/js/2.7d0bb843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba41fcace7cb0b2c4ea3",
+    "url": "/guidance-task/static/js/main.376d0a39.chunk.js"
+  },
+  {
+    "revision": "aefb4c691011623c60bc",
+    "url": "/guidance-task/static/js/runtime-main.46492312.js"
+  },
+  {
+    "revision": "e49a11ae597504f93afd4eb07096eebf",
+    "url": "/guidance-task/static/media/dot.e49a11ae.svg"
+  },
+  {
+    "revision": "2cdb941639cefea1723c591f0045b482",
+    "url": "/guidance-task/static/media/filter.2cdb9416.svg"
+  },
+  {
+    "revision": "6393b4a416fe4cf78536fe78ad999ef0",
+    "url": "/guidance-task/static/media/folder.6393b4a4.svg"
+  },
+  {
+    "revision": "e408e327e8f0044568a411536aebf8c9",
+    "url": "/guidance-task/static/media/font.e408e327.svg"
+  },
+  {
+    "revision": "d10931b30cda5e20d67e4ebb3b9c3169",
+    "url": "/guidance-task/static/media/grid.d10931b3.svg"
+  },
+  {
+    "revision": "b6456a379580f87b1471b1a9948c6f86",
+    "url": "/guidance-task/static/media/layout.b6456a37.svg"
+  },
+  {
+    "revision": "244ae70f3fd599cd6ee966c5940f2611",
+    "url": "/guidance-task/static/media/photo.244ae70f.svg"
+  },
+  {
+    "revision": "076b54e35e91a210a3923cb0d3730b53",
+    "url": "/guidance-task/static/media/search.076b54e3.svg"
+  },
+  {
+    "revision": "f2d778ca1df6e6c3cd0073bae2dc3d43",
+    "url": "/guidance-task/static/media/template.f2d778ca.svg"
+  },
+  {
+    "revision": "83bcb1b60cd0da0602076e139609614b",
+    "url": "/guidance-task/static/media/upload.83bcb1b6.svg"
+  },
+  {
+    "revision": "632332f15bd044c0fa1b2e5a886b3c44",
+    "url": "/guidance-task/static/media/video.632332f1.svg"
+  }
+]);
